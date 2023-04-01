4d39a22f177f8da34ecba21c30f3e397695a1315
EPONCHINTSEV DMITRII <deponchintsevv@gmail.com>
2023-04-01 21:33:27 +0545

    have errors in urls because have no views, and correct the mistakes in models file

--- django_blog/main_blog/models.py
+++ django_blog/main_blog/models.py
@@ -33,7 +33,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ('created_at',)
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
--- django_blog/main_blog/urls.py
+++ django_blog/main_blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('article/<slug:art_name>/', ''),
+    # path('article/<slug:art_name>/', ''),
 ]
