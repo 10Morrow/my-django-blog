6558abbe7f1590f52f1f29e143c44ad9a46c4f74
EPONCHINTSEV DMITRII <deponchintsevv@gmail.com>
2023-04-01 19:29:46 +0545

    create user app and start code models

--- django_blog/main_blog/models.py
+++ django_blog/main_blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Followers(models.Model):
+    pass
