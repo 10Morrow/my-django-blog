c8eac75c8caa1649a1c71edf639d38005c49b667
EPONCHINTSEV DMITRII <deponchintsevv@gmail.com>
2023-04-04 15:24:04 +0545

    fix redirect error

--- django_blog/main_blog/urls.py
+++ django_blog/main_blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import *
 
+app_name = 'main_blog'
 urlpatterns = [
-    path('', GetForMainPage.as_view()),
+    path('', GetForMainPage.as_view(),name='blog'),
     path('article/<slug:art_name>/', GetArticle.as_view()),
     path('<slug:username>/', GetUserPage.as_view()),
 ]
--- django_blog/main_blog/views.py
+++ django_blog/main_blog/views.py
@@ -12,14 +12,14 @@ class GetForMainPage(View):
     @staticmethod
     def get_base_data_template(category):
         if category:
-            article_list = Article.objects.filter(category_list__name=catalog).order_by('-pub_date')[:10]
+            article_list = Article.objects.filter(category_list__name=category).order_by('-pub_date')[:10]
         else:
             article_list = Article.objects.all().order_by('-pub_date')[:10]
         return article_list
 
     def get(self, request):
         selected_category = request.GET.get('category')
-        if request.user.is_authentificate:
+        if request.user.is_authenticated:
             user = request.user
             subscribed_to = [users.id for users in Followers.objects.filter(subscribed_to = user.id)]
             if subscribed_to:
@@ -40,12 +40,12 @@ class GetForMainPage(View):
             return HttpResponse(f"Список статей: {article_list}")
 
 
-
 class GetArticle(View):
     def get(self, request, art_name):
         article = get_object_or_404(Article, slug=art_name)
-        comments = ''
-        return HttpResponse(f"Статья: {article}")
+        comments = Comment.objects.filter(post=article.id)
+
+        return HttpResponse(f"Статья: {article.title}, комментарии: {comments[0]}")
 
 
 class GetUserPage(View):
@@ -53,5 +53,7 @@ class GetUserPage(View):
         user = get_object_or_404(User, username__iexact=username)
         followers_count = len(Followers.objects.filter(subscribed_to = user.id))
         written_articles = Article.objects.filter(author=user.id)
-        profile_data = {"username":user.username, "first_name":user.first_name, "second_name":user.last_name, "folowers_count":followers_count, "wrinnte_art":written_articles}
+        profile_data = {"username": user.username, "first_name": user.first_name,
+                        "second_name": user.last_name, "followers_count": followers_count,
+                        "written_articles": written_articles}
         return HttpResponse(f"информация о пользователе {user.username}: {profile_data}")
