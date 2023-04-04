fe12643fabfe009600046922a6974ecb96fc8061
EPONCHINTSEV DMITRII <deponchintsevv@gmail.com>
2023-04-04 15:23:05 +0545

    write custom registration

--- django_blog/users/forms.py
+++ django_blog/users/forms.py
@@ -15,3 +15,21 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'first_name', 'last_name')
+
+
+class RegistrationUserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'password_confirm')
+
+    def clean(self):
+        cleaned_data = super(RegistrationUserForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('password_confirm')
+        if password and confirm_password and confirm_password != password:
+            raise forms.ValidationError("Passwords do not match")
+
+        return cleaned_data
--- django_blog/users/views.py
+++ django_blog/users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.views import View
+from .forms import RegistrationUserForm
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegistrationView(View):
+
+    def post(self, request):
+        form = RegistrationUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            new_user = User.objects.create_user(username=username, email=email, first_name=first_name, last_name=last_name, password=password)
+            new_user.save()
+            url = reverse('main_blog:blog')
+            return HttpResponseRedirect(url)
+        else:
+            errors = form.errors
+            return render(request, 'users/registration.html', {'form': form, 'errors': errors})
+
+    def get(self, request):
+        user_form = RegistrationUserForm()
+        return render(request, 'users/registration.html', {'form': user_form})
+
